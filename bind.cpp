9d1d9966ab9875030e5b1f962af0a3133c3ea16d
Phineas Campbell <phineas@phincampbell.com>
2025-09-01 10:19:06 +0100

    First Commit

---
+++ bind.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+	double d = 2.6;
+	double x = 0.0;
+	x = sqrt(d);
+    std::cout << "Hello World!"  << std::endl;
+	std::cout << x << std::endl;
+    return 0;
+}
