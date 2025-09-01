2e5f6aca9bf039e5fabb063736902e821f4b7103
Phineas Campbell <phineas@phincampbell.com>
2025-09-01 11:37:37 +0100

    Should Work

--- bind.cpp
+++ bind.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 #include <cmath>
+//#include "pybind11\pybind.h"
+#include "pybind11\pybind11.h"
+
+int add(int i, int j) {
+    return i + j;
+}
+
+PYBIND11_MODULE(example, m, py::mod_gil_not_used()) {
+    m.doc() = "pybind11 example plugin"; // optional module docstring
+
+    m.def("add", &add, "A function that adds two numbers");
+}
 
 int main() {
 	double d = 2.6;
